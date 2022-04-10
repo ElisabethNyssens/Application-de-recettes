6eb47a52a525b5f85c6668ed41af2c3423b71264
Elisabeth Nyssens <babettenyssens@gmail.com>
2022-04-10 18:33:46 +0200

    retest

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,7 @@
 package view;
 
 public class Main {
-    public static void main(String[] args) {
-        MainWindow mainWindoww = new MainWindow();
+    public static void main(String[] args){
+        MainWindow mainWindow = new MainWindow();
     }
 }
