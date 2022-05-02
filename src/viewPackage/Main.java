52e3ad25c8c8e5704f91c3e80e38789e3343d5ab
ElisabethNyssens <babettenyssens@gmail.com>
2022-05-02 16:13:02 +0200

    setup docker

--- src/viewPackage/Main.java
+++ src/viewPackage/Main.java
@@ -1,7 +1,22 @@
 package view;
 
 public class Main {
+<<<<<<< Updated upstream
     public static void main(String[] args) {
         MainWindow mainWindoww = new MainWindow();
+=======
+    //commandes docker si Modif
+    //docker container rm projetrecettes
+    //docker image rm projetrecettes
+    //docker build -t projetrecettes .
+    //docker run --name projetrecettes -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d projetrecettes
+
+    //sinon
+    //docker start projetrecettes
+    //docker stop projetrecettes
+
+    public static void main(String[] args){
+        MainWindow mainWindow = new MainWindow();
+>>>>>>> Stashed changes
     }
 }
